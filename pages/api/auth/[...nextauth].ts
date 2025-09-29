b869214a86a2fe3091225a36c0a1f737a3d29367
Saqib <saqibiqbal27772@gmail.com>
2025-09-29 20:48:53 +0500

    Refactor authentication setup and update dependencies
    - Removed bcrypt and replaced it with bcryptjs in package.json and package-lock.json
    - Updated import path for authOptions in getCurrentUser action
    - Deleted obsolete NextAuth configuration file from the API directory

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -30,9 +30,7 @@ export const authOptions: AuthOptions = {
         }
 
         const user = await prisma.user.findUnique({
-          where: {
-            email: credentials.email,
-          },
+          where: { email: credentials.email },
         });
 
         if (!user || !user?.hashedPassword) {
@@ -56,12 +54,10 @@ export const authOptions: AuthOptions = {
     signIn: "/",
   },
   debug: process.env.NODE_ENV === "development",
-  session: {
-    strategy: "jwt",
-  },
+  session: { strategy: "jwt" },
   secret: process.env.NEXTAUTH_SECRET,
 };
 
-const handler = NextAuth(authOptions);
+export default NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+
